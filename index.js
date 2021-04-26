bb76d11f6e912362dff7faebfda4cd0f634c5ef4
Sparkles2000 <rucker.anna2000@gmail.com>
2021-04-26 06:29:56 -0500

    *added

---
+++ index.js
@@ -0,0 +1,125 @@
+fetch("https://deezerdevs-deezer.p.rapidapi.com/radio/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/infos", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/editorial/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/track/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/artist/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=Christmas", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/genre/Christmas", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/album/All", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f6bc60eb86mshddf1f1cc13a52f3p1f2001jsnbf0780b9e48a",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
